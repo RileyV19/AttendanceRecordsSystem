7e9859a589f029ffc011f0f1d9780a1b8413d5a1
RileyV19 <92709664+RileyV19@users.noreply.github.com>
2021-10-22 10:56:59 +1000

    Core - GUI
    Added comments and split sections

--- Core - GUI.py
+++ Core - GUI.py
@@ -1,12 +1,18 @@
-#Imports
+# Imports
 import sqlite3
-#GUI Imports
+
+# GUI Imports
 import tkinter
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 
-
+#+------------------------------------------------
+#|  Main App class with GUI
+#|    - Manipulate and enter data with a Graphic
+#|      User Interface with buttons and text
+#|      fields
+#+------------------------------------------------
 class App(tkinter.Frame):
     def __init__(self, master=None):
         #Variables
@@ -39,25 +45,27 @@ class App(tkinter.Frame):
 
 
 
-######################################################################
-        ###GUI OPERATIONS
+    #+---------------------------------------
+    #|  GUI Functions - Enter and Query data with a form-like experience
+    #+---------------------------------------
 
-
+    # Clear GUI widgets from background
     def clear_frame(self):
         for widgets in self.GUIRoot.winfo_children():
             widgets.destroy()
 
-
+    # Sign-in page
     def SignInPage(self):
         
         ttk.Label(self.GUIRoot, text="Welcome, please login").grid(column=25, row=3)
         
+        # Password box
         #self.text = StringVar()
         #ttk.Entry(textvar=self.text).grid(column=1, row=2)
         
         ttk.Button(self.GUIRoot, text="Sign In", command=lambda:[self.clear_frame(),self.MainMenu()]).grid(column=25, row=5)
 
-
+    # Main Menu
     def MainMenu(self):
         ttk.Label(self.GUIRoot, text="Welcome, please choose an option: ").grid(column=2, row=0, columnspan=1, padx=10, pady=10)
         ttk.Button(self.GUIRoot, text="Add Student", command=lambda:[self.clear_frame(),self.Add_Student_Menu()]).grid(column=0, row=2, padx=10, pady=10)
@@ -65,33 +73,39 @@ class App(tkinter.Frame):
         ttk.Button(self.GUIRoot, text="Record Parts, Topics or Tests", command=lambda:[self.clear_frame(),self.RecordPartsTopicsTestsMenu()]).grid(column=2, row=2, padx=10, pady=10)
         ttk.Button(self.GUIRoot, text="Search Student Details and Achievements", command=lambda:[self.clear_frame(),self.SearchStudentDetailsAndAchievementsMenu()]).grid(column=3, row=2, padx=10, pady=10)
 
-
+    
+    # Add Student menu
     def Add_Student_Menu(self):
         ttk.Label(self.GUIRoot, text="Please enter new Students Details: ").grid(column=1, row=0, padx=5, pady=5)
         
+        # First Name
         ttk.Label(self.GUIRoot, text="First Name: ").grid(column=0, row=2, padx=5, pady=5)
         self.text1 = StringVar()
         ttk.Entry(self.GUIRoot, textvar=self.text1).grid(column=1, row=2, padx=5, pady=5)
-
+        
+        # Last Name
         ttk.Label(self.GUIRoot, text="Last Name: ").grid(column=0, row=4, padx=5, pady=5)
         self.text2 = StringVar()
         ttk.Entry(self.GUIRoot, textvar=self.text2).grid(column=1, row=4, padx=5, pady=5)
-
+        
+        # DOB
         ttk.Label(self.GUIRoot, text="DOB (YYYY-MM-DD): ").grid(column=0, row=6, padx=5, pady=5)
         self.text3 = StringVar()
         ttk.Entry(self.GUIRoot, textvar=self.text3).grid(column=1, row=6, padx=5, pady=5)
         
+        # Submit Button
         ttk.Button(self.GUIRoot, text="Submit", command=lambda:[self.SubmitStudent(self.text1.get(), self.text2.get(), self.text3.get())]).grid(column=1, row=7, padx=5, pady=5)
-
+        
+        # Main Menu/Back Button
         ttk.Button(self.GUIRoot, text="Main Menu", command=lambda:[self.clear_frame(),self.MainMenu()]).grid(column=0, row=45, padx=5, pady=5)
 
-
+    # Background submission function - Add Student
     def SubmitStudent(self, a: str, b: str, c: str):
         self.cursor.execute("INSERT INTO Student VALUES (?,?,?,?);", (None, a, b, c))
         self.Commit()
 
 
-
+    # Record Attendance menu
     def Record_Attendence_Menu(self):
         ttk.Label(self.GUIRoot, text="Please enter new attendence record: ").grid(column=2, row=0, padx=5, pady=5)
         
@@ -107,13 +121,13 @@ class App(tkinter.Frame):
 
         ttk.Button(self.GUIRoot, text="Main Menu", command=lambda:[self.clear_frame(),self.MainMenu()]).grid(column=1, row=45, padx=5, pady=5)
 
-
+    # Background submission function - Record Attendance
     def SubmitAttendence(self, a: str, b: str):
         self.cursor.execute("INSERT INTO ATTENDENCE VALUES (?,?);", (a, b))
         self.Commit()
 
 
-
+    # Score Students' progression in assessment
     def RecordPartsTopicsTestsMenu(self):
         ttk.Label(self.GUIRoot, text="Please enter new attendence record: ").grid(column=1, row=0, columnspan=1, padx=5, pady=5)
         
@@ -129,12 +143,13 @@ class App(tkinter.Frame):
 
         ttk.Button(self.GUIRoot, text="Main Menu", command=lambda:[self.clear_frame(),self.MainMenu()]).grid(column=10, row=10, padx=10, pady=10, sticky='SE')
 
-
+    # Background submission function - Score Students
     def SubmitPartsTopicsTests(self, a: str, b: str):
         self.cursor.execute("INSERT INTO Student VALUES (?,?);", (a, b))
         self.Commit()
 
     
+    # Search Student Menu
     def SearchStudentDetailsAndAchievementsMenu(self):
         ttk.Label(self.GUIRoot, text="Please enter Student Name to search: ").grid(column=2, row=0, padx=5, pady=5)
         
@@ -150,30 +165,32 @@ class App(tkinter.Frame):
 
         ttk.Button(self.GUIRoot, text="Main Menu", command=lambda:[self.clear_frame(),self.MainMenu()]).grid(column=1, row=45, padx=10, pady=10, sticky="W")
 
-
+    # Background search function - Student details
     def GetSearchResult(self, a: str, b: str):
         #self.StudentSearchResult = self.cursor.execute("SELECT studentID, DOB FROM Student WHERE firstName = '{}' AND lastName = '{}';".format(str(a), str(b))).fetchall()
         self.StudentSearchResult = self.cursor.execute("SELECT studentID, DOB, name, tier, type FROM Student, Badge WHERE Badge.badgeID = StudentBadge.badgeID AND Student.studentID = StudentBadge.studentID AND firstName = '{}' AND lastName = '{}';".format(str(a), str(b)))
         print(self.StudentSearchResult)
 
-
+    # Print Results to the GUI
     def DisplaySearchResult(self):
         print(self.StudentSearchResult)
         #tkinter.messagebox.showinfo(self.StudentSearchResult)
         ttk.Label(self.GUIRoot, text=self.StudentSearchResult).grid(column=1, row=5, columnspan=1, padx=5, pady=5)
 
         
-######################################################################
-        ###SQL OPERATIONS METHODS
-
+    #+---------------------------------------
+    #|  SQL functions - Create, delete, modify, show tables and entries
+    #+---------------------------------------
+    
+    # Create table
     def CreateTable(self, Name: str, Contents: str):
         self.cursor.execute('''CREATE TABLE {} ({});'''.format(Name, Contents))
 
-
+    # Delete Table
     def DeleteTable(self, Name: str):
         self.cursor.execute('''DROP TABLE {};'''.format(Name))
 
-
+    # Show tables
     def ShowTables(self):
         newline_indent = '\n   '
         result = self.cursor.execute('''SELECT name FROM sqlite_master WHERE type='table';''').fetchall()
@@ -184,17 +201,21 @@ class App(tkinter.Frame):
             column_names = list(zip(*result))[1]
             print("\t" + (table_name) + "\t" + str(column_names))
 
-
+    # Show table contents
     def ShowTableContents(self, Name: str):
         newline_indent = '\n   '
         result = self.cursor.execute('''SELECT * FROM {};'''.format(Name)).fetchall()
         print(result)
 
-
+    # Add data to table
     def InsertIntoTable(self, Name: str, Columns: str, Contents: str):
         self.cursor.execute('''INSERT INTO {}({}) VALUES ({});'''.format(Name, Columns, Contents))
 
-
+    
+    #+---------------------------------------
+    #|  SQL connection functions - Establish and close the connection, prepare the cursor
+    #+---------------------------------------
+    
     def Commit(self):
         self.DatabaseConnection.commit()
 
@@ -212,8 +233,9 @@ class App(tkinter.Frame):
         self.cursor = self.DatabaseConnection.cursor()
 
 
-########################################################################
-        ###Selection Methods
+    #+---------------------------------------
+    #|  Command-line User Interface - create and manipulate data from command line
+    #+---------------------------------------
 
     def Selections(self):
         print("\nPlease select one of the following options: {}".format(self.Options))
@@ -264,10 +286,8 @@ class App(tkinter.Frame):
         self.Commit()
         #self.Selections()
         
-#########################################
-        ###MAIN METHODS
 
-
+####################################################################################################
 def main():
     AppInstance = App()
     AppInstance.Connect()
